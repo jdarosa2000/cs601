9ffc0009854b17f7f48c3ffbce17a43d38fb27d7
jdarosa2000 <jdarosa2000@hotmail.com>
2013-12-13 10:16:51 -0500

    simplified submit recipe

--- php/insert.php
+++ php/insert.php
@@ -9,7 +9,7 @@ if (mysqli_connect_errno())
 
 $sql="INSERT INTO recipe (recipe_title, recipe_desc, user_email, categoryId, recipe_steps)
 VALUES
-('$_POST[recipe_title]','$_POST[recipe_desc]','$_POST[recipe_user]','$_POST[recipe_category]','$_POST[recipe_instructions]')";
+('$_POST[recipe_title]','$_POST[recipe_desc]', '$_POST[recipe_user]','$_POST[recipe_category]','$_POST[recipe_instructions]')";
 
 /*
 $sql = "INSERT INTO `cs601`.`recipe` (`recipeId`, `userId`, `recipe_title`, `recipe_desc`, `recipe_steps`, `categoryId`, `tagId`, `recipe_image`) VALUES (\'1\', \'1\', \'kk\', \'kk\', \'kk\', \'1\', NULL, NULL);";
